54cda51db1da4149f3db430a829ae0115b707aba
Yuki Takei <yuki@weseek.co.jp>
2024-12-06 13:22:12 +0000

    add devcontainer files

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+# Setup pnpm
+SHELL=bash pnpm setup
+eval "$(cat /home/vscode/.bashrc)"
+
+# Install global packages
+pnpm install turbo @devcontainers/cli act --global
+
+# Install dependencies
+# turbo run bootstrap
