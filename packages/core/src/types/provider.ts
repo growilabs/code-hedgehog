09147cf0416c23ab38bdac19641476746492368c
Yuki Takei <yuki@weseek.co.jp>
2024-12-06 20:49:05 +0000

    add acme provider

--- packages/core/src/types/provider.ts
+++ packages/core/src/types/provider.ts
@@ -1,11 +1,13 @@
 import type { IFileChange } from './file';
+import type { IPullRequestInfo } from './github';
 import type { IReviewComment } from './review';
 
 export interface IReviewProvider {
   /**
    * Performs code review on file changes
+   * @param prInfo Pull request information
    * @param files List of file changes to review
    * @returns Review comments
    */
-  review(files: IFileChange[]): Promise<IReviewComment[]>;
+  reviewBatch(prInfo: IPullRequestInfo, files: IFileChange[]): Promise<IReviewComment[]>;
 }
---
+++ packages/providers/acme/src/acme.ts
@@ -0,0 +1,13 @@
+import type { IFileChange, IPullRequestInfo, IReviewComment, IReviewProvider } from '@code-hedgehog/core';
+
+export class AcmeReviewProvider implements IReviewProvider {
+  async reviewBatch(prInfo: IPullRequestInfo, files: IFileChange[]): Promise<IReviewComment[]> {
+    // simply comment on each file
+    return files.map((file) => ({
+      path: file.path,
+      position: 1,
+      body: `[ACME] Reviewed ${file.path}\n\nPR: ${prInfo.title}`,
+      type: 'inline',
+    }));
+  }
+}
