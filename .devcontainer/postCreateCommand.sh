04d1d48a83f7c5d59d9d686fb126b22634c97a65
Yuki Takei <yuki@weseek.co.jp>
2025-03-12 12:45:26 +0000

    update devcontainer configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,12 +1,5 @@
-# Setup pnpm
-SHELL=bash pnpm setup
-eval "$(cat /home/vscode/.bashrc)"
-
 # Install act
 curl -s https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
-# Install global packages
-pnpm install turbo @devcontainers/cli act --global
-
 # Install dependencies
-turbo run bootstrap
+deno install
