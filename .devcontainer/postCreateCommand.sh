dd8be16d45ed376e3dc5bf54d9d41daff0cd7138
Yuki Takei <yuki@weseek.co.jp>
2025-04-09 06:09:22 +0000

    update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 # Install act
-curl -s https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
 # Install dependencies
 deno install
