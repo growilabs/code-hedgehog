b2c2e7eb3098bd51f8286a0df9d598592b2b3e41
Yuki Takei <yuki@weseek.co.jp>
2025-04-11 12:24:58 +0000

    add fetch mcp

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,6 @@
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 # Install act
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
