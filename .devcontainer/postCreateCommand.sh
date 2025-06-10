d7ef199a72321a1dbc21432a24dcaa5daf668d95
1129-tame <haruka-tm@weseek.co.jp>
2025-06-10 10:53:16 +0000

    refactor: Update Deno installation script and improve cache directory handling

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,4 +5,4 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
 # Install dependencies
-deno install
+curl -fsSL https://deno.land/x/install/install.sh | sh -s v2.3.5
